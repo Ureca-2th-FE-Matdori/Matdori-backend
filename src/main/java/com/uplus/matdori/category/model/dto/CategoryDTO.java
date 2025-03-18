950a9736b768572a2fe154c8109e34e5a5938d41
gichulLimitLess <hanascreen@konkuk.ac.kr>
2025-03-18 10:38:52 +0900

    FIX: 초기 설정 변경(Lombok 적용 / UserDAO, UserDTO에 특정 카테고리 ID를 가지고 해당 컬럼 불러오는 getter 함수 추가)

--- src/main/java/com/uplus/matdori/category/model/dto/CategoryDTO.java
+++ src/main/java/com/uplus/matdori/category/model/dto/CategoryDTO.java
@@ -1,41 +1,17 @@
 package com.uplus.matdori.category.model.dto;
 
+import lombok.*;
+
 import java.io.Serializable;
 
+//lombok을 사용해서 Getter, Setter 바로 그냥 생성함
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class CategoryDTO implements Serializable {
 	private int category_id;
 	private String category_name;
-
-	// 기본 생성자
-	public CategoryDTO() {}
-
-	// 모든 필드를 포함한 생성자
-	public CategoryDTO(int category_id, String category_name) {
-		this.category_id = category_id;
-		this.category_name = category_name;
-	}
-
-	// Getter & Setter
-	public int getCategory_id() {
-		return category_id;
-	}
-
-	public void setCategory_id(int category_id) {
-		this.category_id = category_id;
-	}
-
-	public String getCategory_name() {
-		return category_name;
-	}
-
-	public void setCategory_name(String category_name) {
-		this.category_name = category_name;
-	}
-
-	// toString() 메서드
-	@Override
-	public String toString() {
-		return "CategoryDTO { category_id = " + category_id + ", category_name = '" + category_name + "' }";
-	}
 }
 
--- src/main/java/com/uplus/matdori/category/model/dto/UserDTO.java
+++ src/main/java/com/uplus/matdori/category/model/dto/UserDTO.java
@@ -1,50 +1,35 @@
 package com.uplus.matdori.category.model.dto;
 
+import lombok.*;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
+//lombok을 사용해서 Getter, Setter 바로 그냥 생성함
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class UserDTO {
     private String userId;  // 회원 ID (VARCHAR 10)
     private int point;  // 포인트 (INT)
     private String password;  // 비밀번호 (VARCHAR 16)
     private List<Integer> categoryVisits;  // 카테고리별 방문 횟수 (C_1 ~ C_15)
 
-    // 기본 생성자
-    public UserDTO() {
-        // 초기값을 0으로 설정된 리스트 생성 (카테고리 15개)
-        this.categoryVisits = new ArrayList<>(Collections.nCopies(15, 0));
-    }
-
-    // 모든 필드를 포함한 생성자
-    public UserDTO(String userId, int point, String password, List<Integer> categoryVisits) {
-        this.userId = userId;
-        this.point = point;
-        this.password = password;
-        this.categoryVisits = categoryVisits != null ? categoryVisits : new ArrayList<>(Collections.nCopies(15, 0));
-    }
-
-    // Getter & Setter
-    public String getUserId() { return userId; }
-    public void setUserId(String userId) { this.userId = userId; }
-
-    public int getPoint() { return point; }
-    public void setPoint(int point) { this.point = point; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
+    //카테고리별 방문 횟수 조회
     public List<Integer> getCategoryVisits() { return categoryVisits; }
     public void setCategoryVisits(List<Integer> categoryVisits) {
         this.categoryVisits = categoryVisits != null ? categoryVisits : new ArrayList<>(Collections.nCopies(15, 0));
     }
 
-    // 특정 카테고리 방문 횟수 조회
+    // 특정 카테고리 방문 횟수 조회 (매개변수로 카테고리 id를 넣으면 됨)
     public int getCategoryVisitCount(int categoryIndex) {
         if (categoryIndex < 1 || categoryIndex > 15) {
             throw new IndexOutOfBoundsException("Category index must be between 1 and 15.");
         }
-        return categoryVisits.get(categoryIndex - 1);
+        return categoryVisits.get(categoryIndex - 1); //categoryId는 1~15의 값을, 리스트는 0~14의 값을 사용한다
     }
 
     // 특정 카테고리 방문 횟수 변경
