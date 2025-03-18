6e485027a3ca499c77d15965a706551d580a16f5
gichulLimitLess <hanascreen@konkuk.ac.kr>
2025-03-18 15:43:51 +0900

    feat: 네이버 지역검색 API 연결 (추후 프론트엔드에서 현재 위치를 받아와서 개선해야 함)

--- src/main/java/com/uplus/matdori/category/model/service/SelectService.java
+++ src/main/java/com/uplus/matdori/category/model/service/SelectService.java
@@ -1,8 +1,9 @@
 package com.uplus.matdori.category.model.service;
 
 import com.uplus.matdori.category.model.dto.CategoryDTO;
+import com.uplus.matdori.category.model.dto.NaverLocalResponseDTO;
 
 public interface SelectService {
-    String getRandomCategory();
+    NaverLocalResponseDTO getRandomCategory();
     String getPreferredCategory(String userId);
 }
