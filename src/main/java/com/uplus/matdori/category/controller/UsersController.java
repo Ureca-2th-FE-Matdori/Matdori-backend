f2c91eb9bb54120c0d8f1b03fbdf58febbba7b8a
choi-hi <gmltmdsp@hanmail.net>
2025-03-18 17:46:42 +0900

    feat : 회원가입 기능 추가

--- src/main/java/com/uplus/matdori/category/controller/UsersController.java
+++ src/main/java/com/uplus/matdori/category/controller/UsersController.java
@@ -1,30 +1,23 @@
 package com.uplus.matdori.category.controller;
 
-import com.uplus.matdori.category.model.dto.UserDTO;
-import com.uplus.matdori.category.model.service.UsersService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-/*
- * @RestController
- *   RestFul Service를 위한 Conrtoller
- *   모든 메서드의 응답을  @ResponseBody를 붙여주는 효과
- */
+import com.uplus.matdori.category.model.dto.ApiResponse;
+import com.uplus.matdori.category.model.dto.UserDTO;
+import com.uplus.matdori.category.model.dto.UserResponseDto;
+import com.uplus.matdori.category.model.service.UsersService;
+
+
 @RestController
 
-//RestFul에서 서비스할 자원(Domain)명을 붙인다
 @RequestMapping("/users")
 
-/*
- * @CrossOrigin
- *  - CORS 요청에 대한 승인
- *  origins = {"*"}  : 요청하는 모든 URL, 메서드를 승인
- *     ==> 보안에 취약하므로 상용에서는 사용 안함
- *     ==> 이후에 Configuration을 통해 설정할 예정
- * */
 @CrossOrigin(origins = {"*"})
 
 
@@ -32,7 +25,6 @@ import org.springframework.web.bind.annotation.*;
 public class UsersController {
     private final UsersService usersService;
 
-    @Autowired
     public UsersController(UsersService usersService) {
         this.usersService = usersService;
     }
@@ -40,14 +32,14 @@ public class UsersController {
     //로그인
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody UserDTO userDTO) {
-        boolean success = usersService.login(userDTO.getUserId(), userDTO.getPassword());
+        boolean success = usersService.login(userDTO.getUser_id(), userDTO.getPassword());
         return success ? ResponseEntity.ok("Login Success") : ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login Failed");
     }
 
     //회원가입(POST)
     @PostMapping("/createAccount")
-    public ResponseEntity<String> createAccount(@RequestBody UserDTO userDTO) {
-        usersService.createAccount(userDTO);
-        return ResponseEntity.ok("Account Created Successfully");
+    public ResponseEntity<ApiResponse<UserResponseDto>> createAccount(@RequestBody UserDTO userDTO) {
+        return usersService.createAccount(userDTO);
     }
+   
 }
--- src/main/java/com/uplus/matdori/category/model/service/UsersServiceImp.java
+++ src/main/java/com/uplus/matdori/category/model/service/UsersServiceImp.java
@@ -1,14 +1,20 @@
 package com.uplus.matdori.category.model.service;
 
-import com.uplus.matdori.category.model.dao.UserDAO;
-import com.uplus.matdori.category.model.dto.UserDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.uplus.matdori.category.model.dao.UserDAO;
+import com.uplus.matdori.category.model.dto.ApiResponse;
+import com.uplus.matdori.category.model.dto.UserDTO;
+import com.uplus.matdori.category.model.dto.UserResponseDto;
 
 //회원 정보 관련 기능들을 구현한 UsersServiceImp
 @Service
 public class UsersServiceImp implements UsersService {
     private final UserDAO userDAO;
-
+    
+	
     public UsersServiceImp(UserDAO userDAO) {
         this.userDAO = userDAO;
     }
@@ -17,8 +23,44 @@ public class UsersServiceImp implements UsersService {
         UserDTO user = userDAO.getUserById(userId);
         return user != null && user.getPassword().equals(password);
     }
-
-    public void createAccount(UserDTO userDTO) {
-        userDAO.insertUser(userDTO);
+   
+    // 아이디 중복 확인
+    public boolean checkUserId(String userId) {
+    	UserDTO user = userDAO.getUserById(userId);
+    	
+    	if (user == null) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
+    
+    
+    // 회원가입
+    public ResponseEntity<ApiResponse<UserResponseDto>> createAccount(UserDTO userDTO) {
+
+    	if(checkUserId(userDTO.getUser_id())) {
+    		// id가 null 값이 맞다면 가입 처리 진행
+    		// 정보 삽입
+    		try {
+    			userDAO.insertUser(userDTO);
+    			
+    			UserResponseDto responseDto = new UserResponseDto(userDTO.getUser_id());
+    			return ResponseEntity.ok(ApiResponse.success(responseDto));
+    			
+    		} catch (Exception e) {
+    	         throw new RuntimeException(e);  // 글로벌 예외 처리로 500 에러 반환
+    	    }
+    		
+    	} else {
+    		// 가입 실패 : 아이디 중복
+    		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(ApiResponse.error("이미 존재하는 아이디입니다!"));
+    	}
+
+        
+    }
+  
+    
+
 }
