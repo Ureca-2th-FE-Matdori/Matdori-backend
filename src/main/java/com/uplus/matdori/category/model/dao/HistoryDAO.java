58228ff6d789d8664ebb0c81fa1b8cdb0485405b
0Jaemin0 <jaemin2167@naver.com>
2025-03-21 01:12:48 +0900

    feat: history 조 및 삭제를 위한 MyBatis Mapping 추가

--- src/main/java/com/uplus/matdori/category/model/dao/HistoryDAO.java
+++ src/main/java/com/uplus/matdori/category/model/dao/HistoryDAO.java
@@ -1,6 +1,8 @@
 package com.uplus.matdori.category.model.dao;
 
 import com.uplus.matdori.category.model.dto.HistoryDTO;
+import com.uplus.matdori.category.model.dto.HistoryRequestDTO;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import java.util.List;
@@ -10,4 +12,6 @@ public interface HistoryDAO {
 
     //방문한 식당 정보를 기록
     void insertVisitHistory(HistoryDTO visitHistory);
+    public HistoryDTO getHistoryByUserIdAndHistoryId(HistoryRequestDTO requestDTO);
+    void deleteHistory(int history_id);
 }
