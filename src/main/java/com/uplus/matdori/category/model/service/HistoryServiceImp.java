67e43612e3c6bb72d9c57436d9fcdf1f8d995e48
gichulLimitLess <hanascreen@konkuk.ac.kr>
2025-03-20 17:03:28 +0900

    feat: history_id 기반으로 특정 히스토리의 평점 업데이트하는 기능 추가

--- src/main/java/com/uplus/matdori/category/model/service/HistoryServiceImp.java
+++ src/main/java/com/uplus/matdori/category/model/service/HistoryServiceImp.java
@@ -1,12 +1,21 @@
 package com.uplus.matdori.category.model.service;
 
 import com.uplus.matdori.category.model.dao.HistoryDAO;
+import com.uplus.matdori.category.model.dto.ApiResponse;
 import com.uplus.matdori.category.model.dto.HistoryDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
+
+import static java.rmi.server.LogStream.log;
 
 //History 관련 정보들을 처리하는 기능을 가진 HistoryServiceImp
+@Slf4j
 @Service
 public class HistoryServiceImp implements HistoryService {
     private final HistoryDAO historyDAO;
@@ -20,13 +29,34 @@ public class HistoryServiceImp implements HistoryService {
         return List.of();
     }
 
+    //특정 방문 내역의 평점(rate)을 업데이트하는 메소드
     @Override
-    public void rateHistory(HistoryDTO historyDTO) {
+    @Transactional // 데이터 일관성을 위해 트랜잭션 적용
+    public ResponseEntity<ApiResponse<Object>> rateHistory(int history_id, int rate) {
+        // history_id가 유효한지 조회
+        HistoryDTO history = historyDAO.findById(history_id);
 
+        if (history == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error("유효하지 않은 history_id 값이에요!"));
+        }
+
+        log.info(history.toString());
+
+        // rate 값이 0~5 범위를 벗어나면 예외 발생
+        if (rate < 0 || rate > 5) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error("평점은 0~5사이어야 하잖아 이자쉭아"));
+        }
+
+        // 기존 데이터의 rate를 업데이트
+        history.setRate(rate);
+        historyDAO.updateRating(history); // DB에 반영
+
+        //success일 경우에는 Map.of() 넘겨서.. content는 빈 객체를 넘김
+        return ResponseEntity.ok(ApiResponse.success(Map.of()));
     }
 
     @Override
     public void deleteHistory(int historyId) {
-
+        //아직 구현 안해놓음
     }
 }
