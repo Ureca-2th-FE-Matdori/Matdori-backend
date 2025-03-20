93c6fb923956cdb75ca28aadbb0ef2b72ec40384
cm2hcm2h <cm2hcm2h@naver.com>
2025-03-20 11:01:13 +0900

    feat: 특정 회원의 랭킹 조회 서비스 구현

--- src/main/java/com/uplus/matdori/category/model/service/RankingServiceImp.java
+++ src/main/java/com/uplus/matdori/category/model/service/RankingServiceImp.java
@@ -3,9 +3,13 @@ package com.uplus.matdori.category.model.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.uplus.matdori.category.model.dao.UserDAO;
+import com.uplus.matdori.category.model.dto.ApiResponse;
+import com.uplus.matdori.category.model.dto.MyRankingDTO;
 import com.uplus.matdori.category.model.dto.UserDTO;
 import com.uplus.matdori.category.model.dto.UserRankingDTO;
 
@@ -27,4 +31,19 @@ public class RankingServiceImp implements RankingService {
         		.map(user -> new UserRankingDTO(user.getUser_id(), user.getPoint()))
         		.collect(Collectors.toList());
     }
+
+	@Override
+	public ResponseEntity<ApiResponse<MyRankingDTO>> getMyRanking(String user_id) {
+		try {
+			MyRankingDTO myRanking = userDAO.getMyRanking(user_id);
+			// 존재하지 않는 회원의 랭킹을 조회한 경우
+			if(myRanking == null) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error("존재하지 않는 회원입니다."));
+			}
+			// 존재하는 회원의 랭킹을 조회한 경우
+			return ResponseEntity.ok(ApiResponse.success(myRanking));
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
